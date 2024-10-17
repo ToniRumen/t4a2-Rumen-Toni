1affa8f8965c7e276d4365b7c50a1f0a4f0c724e
ToniRumen <tonidam24@gmail.com>
2024-10-17 22:08:18 +0200

    Proyecto al 98%

--- app/src/main/java/com/example/t3a3_rumen_toni/LoginActivity.kt
+++ app/src/main/java/com/example/t3a3_rumen_toni/LoginActivity.kt
@@ -1,16 +1,48 @@
 package com.example.t3a3_rumen_toni
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.t3a3_rumen_toni.databinding.ActivityLoginBinding
+
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        //Inicializo el binding
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Evento al pulsar botón "Entrar"
+
+        binding.botonEntrar.setOnClickListener {
+
+            //Ahora el intent --> Comunicación entre ficheros
+
+            val intent = Intent(this, MainActivity::class.java)
+
+            intent.putExtra("usuario",binding.textoUsuario.text.toString())
+
+            startActivity(intent)
+        }
+
+        binding.botonSalir.setOnClickListener {
+
+            finishAffinity()
+
+
+        }
+
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/com/example/t3a3_rumen_toni/MainActivity.kt
+++ app/src/main/java/com/example/t3a3_rumen_toni/MainActivity.kt
@@ -5,12 +5,30 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.t3a3_rumen_toni.databinding.ActivityLoginBinding
+import com.example.t3a3_rumen_toni.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        //Inicializar binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Recuperar valor
+        val numUsuarioRecibido = intent.getStringExtra("usuario")
+
+        //Mostrar resultado
+        binding.textoBienvenida.text = "Bienvenido/a \n $numUsuarioRecibido"
+
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
