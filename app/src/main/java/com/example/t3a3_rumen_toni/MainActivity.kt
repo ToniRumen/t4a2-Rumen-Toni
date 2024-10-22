08b1724873c1ec2da235dcd53a8e69d5fd5f5df3
ToniRumen <tonidam24@gmail.com>
2024-10-23 00:01:55 +0200

    Añadir iconos

--- app/src/main/java/com/example/t3a3_rumen_toni/MainActivity.kt
+++ app/src/main/java/com/example/t3a3_rumen_toni/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.t3a3_rumen_toni
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -24,6 +25,40 @@ class MainActivity : AppCompatActivity() {
 
         //Mostrar resultado
         binding.textoBienvenida.text = "Bienvenido/a \n $numUsuarioRecibido"
+
+
+
+        //Botón "contraseña" --> A la pantalla Login
+
+
+        binding.botonContrasena.setOnClickListener {
+
+            //Intent
+
+            val intent = Intent(this, LoginActivity::class.java)
+
+            startActivity(intent)
+        }
+
+
+
+
+
+        //Botón "Salir" --> A la pantalla principal
+
+        binding.botonSalir.setOnClickListener {
+
+            //Intent
+
+            val intent = Intent(this, WelcomeActivity::class.java)
+
+            startActivity(intent)
+        }
+
+        // Le tengo que dar 40 veces al boton de iniciar
+        // para que me entre aqui, o pasar por los textos.
+
+
 
 
         super.onCreate(savedInstanceState)
